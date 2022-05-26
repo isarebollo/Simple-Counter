dd0d24a25045c37dc212dd65f41405d30e9c3699
isarebollo <isarebollo@gmail.com>
2022-05-26 08:13:42 +0000

    Simple Counter Isabel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-
 import { counter } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 import ReactDOM from "react-dom";
@@ -11,14 +10,20 @@ function SimpleCounter(props) {
 			<div className="clock">
 				<i className="far fa-clock"></i>
 			</div>
+			<div className="six">{props.digitSix % 10}</div>
+			<div className="five">{props.digitFive % 10}</div>
+			<div id="puntos"> : </div>
 			<div className="four">{props.digitFour % 10}</div>
 			<div className="three">{props.digitThree % 10}</div>
+			<div id="puntos"> : </div>
 			<div className="two">{props.digitTwo % 10}</div>
 			<div className="one">{props.digitOne % 10}</div>
 		</div>
 	);
 }
 SimpleCounter.propTypes = {
+	digitSix: PropTypes.number,
+	digitFive: PropTypes.number,
 	digitFour: PropTypes.number,
 	digitThree: PropTypes.number,
 	digitTwo: PropTypes.number,
@@ -26,6 +31,8 @@ SimpleCounter.propTypes = {
 };
 let count = 0;
 setInterval(function () {
+	const six = Math.floor(count / 100000);
+	const five = Math.floor(count / 10000);
 	const four = Math.floor(count / 1000);
 	const three = Math.floor(count / 100);
 	const two = Math.floor(count / 10);
@@ -39,6 +46,8 @@ setInterval(function () {
 			digitTwo={two}
 			digitThree={three}
 			digitFour={four}
+			digitFive={five}
+			digitSix={six}
 		/>,
 		document.querySelector("#app")
 	);
